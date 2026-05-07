17f37107d33d246edf3dbf36ddd38771d9536658
mannchwint <.@aleksuser.com>
2026-05-07 16:21:38 +0500

    Добавьте файлы проекта.

---
+++ RISI Service Desk/AddEditServicePage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RISI_Service_Desk
+{
+    /// <summary>
+    /// Логика взаимодействия для AddEditServicePage.xaml
+    /// </summary>
+    public partial class AddEditServicePage : Page
+    {
+        private Service _currentServices = new Service();
+
+        public AddEditServicePage(Service selectedServices)
+        {
+            InitializeComponent();
+            if (selectedServices != null)
+                _currentServices = selectedServices;
+            DataContext = _currentServices;
+            CmbNameServise.ItemsSource = RISI_ServiceDeskEntities1.GetContext().Services.ToList();
+        }
+
+        private void BtnSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
